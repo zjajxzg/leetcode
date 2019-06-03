f3bda42974b5bc2c7960aa19fb5db07db68d4cf0
xuzhigang <zjajxzg@163.com>
2019-06-03 20:14:41 +0800

    feat twosum

---
+++ src/exercise/easy/topic007/Reverse.java
@@ -0,0 +1,16 @@
+package exercise.easy.topic007;
+
+/**
+ * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+ */
+public class Reverse {
+
+    // TODO
+    public static int reverse1(int x) {
+       return 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse1(123));
+    }
+}
