2f4d021341d25771f636a16632be27eb121cb51a
xuzhigang <zjajxzg@163.com>
2019-06-05 14:00:43 +0800

    feat Reverse

--- src/exercise/easy/topic001/TwoSum.java
+++ src/exercise/easy/topic001/TwoSum.java
@@ -13,6 +13,12 @@ import java.util.Arrays;
  */
 public class TwoSum {
 
+    /**
+     * O(n2)
+     * @param nums
+     * @param target
+     * @return
+     */
     public int[] twoSum1(int[] nums, int target) {
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
--- src/exercise/easy/topic007/Reverse.java
+++ src/exercise/easy/topic007/Reverse.java
@@ -5,12 +5,39 @@ package exercise.easy.topic007;
  */
 public class Reverse {
 
-    // TODO
+    /**
+     * 先计算出x的长度i   然后for循环i次，每次%10的j次方取余数 再/10的j-1次取出每一位的值  再乘以10的i-j次倒序相加
+     * 注意点（int的范围是-2^31~2^31-1,不仅要考虑x不能超过这个范围    还要考虑反转后的值不能超过这个范围）
+     * 基本就是数学的思路
+     * @param x
+     * @return
+     */
     public static int reverse1(int x) {
-       return 1;
+        if ((long) x > Integer.MAX_VALUE || (long) x < Integer.MIN_VALUE) {
+            return 0;
+        }
+        int i = 1;
+        int xx = x;
+        while (true) {
+            x = x / 10;
+            if (x == 0) {
+                break;
+            }
+            i++;
+        }
+        long z = 0;
+        int num;
+        for (int j = 1; j <= i; j++) {
+            num = xx % (int) (Math.pow(10, j)) / (int) Math.pow(10, j - 1);
+            z += (long) num * (int) (Math.pow(10, (i - j)));
+        }
+        if (z > Integer.MAX_VALUE || z < Integer.MIN_VALUE) {
+            return 0;
+        }
+        return (int) z;
     }
 
     public static void main(String[] args) {
-        System.out.println(reverse1(123));
+        System.out.println(reverse1(12));
     }
 }
