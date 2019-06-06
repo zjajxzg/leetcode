fd5edb8c4945b37d719190cc1999151a38076a3b
xuzhigang <zjajxzg@163.com>
2019-06-06 18:37:19 +0800

    feat IsValid

---
+++ src/exercise/easy/topic020/IsValid.java
@@ -0,0 +1,26 @@
+package exercise.easy.topic020;
+
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串。
+ */
+public class IsValid {
+
+    /**
+     * TODO 栈
+     * @param s
+     * @return
+     */
+    public static boolean isValid(String s) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("{}"));
+    }
+}
