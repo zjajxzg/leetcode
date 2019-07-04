d7ce95640b765aaafa97102ee950419eb02fd9c5
xuzhigang <zjajxzg@163.com>
2019-07-04 17:26:47 +0800

    feat 16

--- src/exercise/swordoffer/_016/Merge.java
+++ src/exercise/swordoffer/_016/Merge.java
@@ -31,32 +31,84 @@ public class Merge {
     /**
      * TODO
      */
-    public static ListNode merge2(ListNode list1, ListNode list2) {
+//    public static ListNode merge2(ListNode list1, ListNode list2) {
+//        if(list1 == null){
+//            return list2;
+//        }
+//        if(list2 == null){
+//            return list1;
+//        }
+//        ListNode list = null;
+//        ListNode current = null;
+//        list = current = list1.val < list2.val ? list1 : list2;
+//        while (list1 != null && list2 != null) {
+//            if (list1.val <= list2.val) {
+//                 current.next = list1;
+//                 current = current.next;
+//                 list1 = list1.next;
+//            } else {
+//                current.next = list1;
+//                current = current.next;
+//                list2 = list2.next;
+//            }
+//        }
+//        if (list1 == null) {
+//            current.next = list2;
+//        } else {
+//            current.next = list1;
+//        }
+//        return list;
+//    }
+
+    /**
+     * 两个链表从头开始比较
+     * @param list1
+     * @param list2
+     * @return
+     */
+    public static ListNode merge3(ListNode list1, ListNode list2) {
         if(list1 == null){
             return list2;
         }
         if(list2 == null){
             return list1;
         }
-        ListNode list = new ListNode(list1.val < list2.val ? list1.val : list2.val);
-        while (list1.next != null && list2.next != null) {
-            if (list1.next.val <= list2.next.val) {
-                 list.next = list1.next;
-                 list1 = list1.next.next;
-            } else {
-                list.next = list2.next;
-                list2 = list2.next.next;
+        ListNode mergeHead = null;
+        ListNode current = null;
+        while(list1!=null && list2!=null){
+            if(list1.val <= list2.val){
+                if(mergeHead == null){
+                    mergeHead = current = list1;
+                }else{
+                    current.next = list1;
+                    current = current.next;
+                }
+                list1 = list1.next;
+            }else{
+                if(mergeHead == null){
+                    mergeHead = current = list2;
+                }else{
+                    current.next = list2;
+                    current = current.next;
+                }
+                list2 = list2.next;
             }
         }
-        return list;
+        if(list1 == null){
+            current.next = list2;
+        }else{
+            current.next = list1;
+        }
+        return mergeHead;
     }
+
 
     public static void main(String[] args) {
         ListNode list1 = new ListNode(1);
         list1.next = new ListNode(3);
         ListNode list2 = new ListNode(2);
         list2.next = new ListNode(4);
-        System.out.println(merge(list1, list2));
-        System.out.println(merge2(list1, list2));
+//        System.out.println(merge(list1, list2));
+        System.out.println(merge3(list1, list2));
     }
 }
