8ea547b802761fc5fc8cf7bbcb30253c57d0f3c2
xuzhigang <zjajxzg@163.com>
2019-07-16 19:56:12 +0800

    feat 283

---
+++ src/exercise/hot100/_283/MoveZeroes.java
@@ -0,0 +1,30 @@
+package exercise.hot100._283;
+
+
+/**
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ *
+ * 示例:
+ *
+ * 输入: [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ * 说明:
+ *
+ * 必须在原数组上操作，不能拷贝额外的数组。
+ * 尽量减少操作次数。
+ */
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int first = 0;
+        int end = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < ) {
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/exercise/hot100/_461/HammingDistance.java
@@ -0,0 +1,27 @@
+package exercise.hot100._461;
+
+/**
+ * 两个整数之间的汉明距离指的是这两个数字对应二进制位不同的位置的数目。
+ *
+ * 给出两个整数 x 和 y，计算它们之间的汉明距离。
+ *
+ * 注意：
+ * 0 ≤ x, y < 2^31.
+ *
+ */
+public class HammingDistance {
+
+    /**
+     * TODO
+     * @param x
+     * @param y
+     * @return
+     */
+    public int hammingDistance(int x, int y) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
