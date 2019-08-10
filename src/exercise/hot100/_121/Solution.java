ae848f8d4f3bce33e64ab657e851ca251c59f3ad
ziajxzg <zjajxzg@163.com>
2019-08-10 16:56:22 +0800

    feat: 121 股票最大收益

---
+++ src/exercise/hot100/_121/Solution.java
@@ -0,0 +1,56 @@
+package exercise.hot100._121;
+
+/**
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ *
+ * 如果你最多只允许完成一笔交易（即买入和卖出一支股票），设计一个算法来计算你所能获取的最大利润。
+ *
+ * 注意你不能在买入股票前卖出股票。
+ *
+ * 示例 1:
+ *
+ * 输入: [7,1,5,3,6,4]
+ * 输出: 5
+ * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+ *      注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
+ * 示例 2:
+ *
+ * 输入: [7,6,4,3,1]
+ * 输出: 0
+ * 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+
+    /**
+     * dp思想   记录前一天的最大收益 后一天的值减去前面的最小值  和最大收益比较  即可更新最大收益
+     * @param prices
+     * @return
+     */
+    public static int maxProfit(int[] prices) {
+        int n = prices.length;
+        if (n <= 1) {
+            return 0;
+        }
+        int min = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < n; i++) {
+            maxProfit = Math.max(prices[i] - min, maxProfit);
+            min = Math.min(min, prices[i]);
+
+        }
+        return maxProfit;
+
+    }
+
+
+
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
+    }
+}
