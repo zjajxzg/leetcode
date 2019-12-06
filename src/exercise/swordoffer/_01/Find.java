dd3c0469841795b016d27fc9d3c6a6ca6e1b7148
zhigang.xu <zhigang@mamahao.com>
2019-12-06 17:16:42 +0800

    临时提交

--- src/exercise/swordoffer/_01/Find.java
+++ src/exercise/swordoffer/_01/Find.java
@@ -9,6 +9,8 @@ package exercise.swordoffer._01;
  */
 public class Find {
     /**
+     * 暴力  遍历所有一维数组
+     *
      * @param target
      * @param array
      * @return
@@ -25,8 +27,57 @@ public class Find {
         return false;
     }
 
+    /**
+     * 暴力+二分（有序的东西有意识的联想到二分）
+     *
+     * @param target
+     * @param array
+     * @return
+     */
+    public static boolean find2(int target, int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (!binarySearch(target, array[i])) {
+                continue;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 二分查找一个数在不在一个有序数组中（非递归版本）
+     * @param target
+     * @param array
+     * @return
+     */
+    private static boolean binarySearch(int target, int[] array) {
+        int right = array.length, left = 0;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (target == array[mid]) {
+                return true;
+            } else if (target < array[mid]) {
+                right = (mid + right) / 2;
+            } else if (target > array[mid]) {
+                left = (mid + left) / 2;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 二分递归版
+     *
+     * @param target
+     * @param array
+     * @return
+     */
+    private static boolean binarySearchRecursive(int target, int[] array) {
+        return true;
+    }
+
     public static void main(String[] args) {
         int[][] array = {{1,2,8,9},{2,4,9,12},{4,7,10,13},{6,8,11,15}};
-        System.out.println(find(7, array));
+        System.out.println(find2(3, array));
     }
 }
