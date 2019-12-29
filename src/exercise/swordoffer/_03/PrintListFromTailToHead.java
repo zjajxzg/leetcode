0c383d3f1c6ab86560d40d6b3771ff29236bfa4a
ziajxzg <zjajxzg@163.com>
2019-12-29 21:15:47 +0800

    feat: 剑指offer第三题 从尾到头打印链表

--- src/exercise/swordoffer/_03/PrintListFromTailToHead.java
+++ src/exercise/swordoffer/_03/PrintListFromTailToHead.java
@@ -8,6 +8,10 @@ import java.util.ArrayList;
  * @author zhigang.xu
  */
 public class PrintListFromTailToHead {
+    /**
+     * @param listNode
+     * @return
+     */
     public static ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
         ArrayList<Integer> arrayList = new ArrayList<>();
         ArrayList<Integer> arrayList1 = new ArrayList<>();
@@ -24,6 +28,41 @@ public class PrintListFromTailToHead {
             arrayList1.add(arrayList.get(i));
         }
         return arrayList1;
+    }
+
+    /**
+     * 头插
+     *
+     * @param listNode
+     * @return
+     */
+    public static ArrayList<Integer> printListFromTailToHead2(ListNode listNode) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        if (listNode == null) {
+            return arrayList;
+        }
+        arrayList.add(listNode.val);
+        while (listNode.next != null) {
+            arrayList.add(0, listNode.next.val);
+            listNode = listNode.next;
+        }
+        return arrayList;
+    }
+
+    private static ArrayList<Integer> arrayList = new ArrayList<>();
+
+    /**
+     * 递归
+     *
+     * @param listNode
+     * @return
+     */
+    public static ArrayList<Integer> printListFromTailToHead3(ListNode listNode) {
+        if (listNode != null) {
+            printListFromTailToHead3(listNode.next);
+            arrayList.add(listNode.val);
+        }
+        return arrayList;
     }
 
     public static void main(String[] args) {
