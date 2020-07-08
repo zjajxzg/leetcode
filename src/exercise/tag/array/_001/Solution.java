c65078b932efe3bb4401e5d8e70411948ca81918
zhigang.xu <zhigang@mamahao.com>
2020-07-08 16:27:10 +0800

    feat: 重新开始刷题 数组001

---
+++ src/exercise/tag/array/_001/Solution.java
@@ -0,0 +1,49 @@
+package exercise.tag.array._001;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ *
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ *
+ *  
+ *
+ * 示例:
+ *
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ *
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author zhigang.xu
+ */
+public class Solution {
+    /**
+     * 暴力 两次for循环 时间复杂度O(n)
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum1(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 4};
+        int target = 6;
+        System.out.println(Arrays.toString(Solution.twoSum1(nums, target)));
+    }
+}
