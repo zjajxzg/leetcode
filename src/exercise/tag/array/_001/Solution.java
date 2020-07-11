9cb50d02c919d21658906df031949d69866d01df
zhigang.xu <zhigang@mamahao.com>
2020-07-11 19:39:49 +0800

    feat: 1. array001 hash优化 2. array026 快慢指针解

--- src/exercise/tag/array/_001/Solution.java
+++ src/exercise/tag/array/_001/Solution.java
@@ -1,21 +1,22 @@
 package exercise.tag.array._001;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 /**
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
- *
+ * <p>
  * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
- *
+ * <p>
  *  
- *
+ * <p>
  * 示例:
- *
+ * <p>
  * 给定 nums = [2, 7, 11, 15], target = 9
- *
+ * <p>
  * 因为 nums[0] + nums[1] = 2 + 7 = 9
  * 所以返回 [0, 1]
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/two-sum
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -34,9 +35,48 @@ public class Solution {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    return new int[] {i, j};
+                    return new int[]{i, j};
                 }
             }
+        }
+        return null;
+    }
+
+    /**
+     * 利用hash  两次循环
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum2(int[] nums, int target) {
+        final HashMap<Integer, Integer> map = new HashMap<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int result = target - nums[i];
+            if (map.containsKey(result) && map.get(result) != i) {
+                return new int[]{i, map.get(result)};
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 利用hash  一次循环
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum3(int[] nums, int target) {
+        final HashMap<Integer, Integer> map = new HashMap<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            if (map.get(target - nums[i]) != null) {
+                return new int[]{i, map.get(target - nums[i])};
+            }
+            map.put(nums[i], i);
         }
         return null;
     }
@@ -45,5 +85,7 @@ public class Solution {
         int[] nums = {3, 2, 4};
         int target = 6;
         System.out.println(Arrays.toString(Solution.twoSum1(nums, target)));
+        System.out.println(Arrays.toString(Solution.twoSum2(nums, target)));
+        System.out.println(Arrays.toString(Solution.twoSum3(nums, target)));
     }
 }
