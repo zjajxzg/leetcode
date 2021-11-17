20fce48451fb1ff262a322f0206741143cd41c72
xuzhigang <xuzhigang@duiba.com.cn>
2021-11-17 21:01:04 +0800

    feat: 2. 两数相加 临时提交

---
+++ src/common/ListNode.java
@@ -0,0 +1,13 @@
+package common;
+
+/**
+ * 连表节点
+ *
+ * @author xuzhigang
+ * @date 2021/11/2 8:34 上午
+ **/
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
