540c5e3623096a316c348a9afb29a296260e69a1
xuzhigang <xuzhigang@duiba.com.cn>
2021-11-18 08:28:11 +0800

    feat: 2. 两数相加

--- src/common/ListNode.java
+++ src/common/ListNode.java
@@ -7,7 +7,7 @@ package common;
  * @date 2021/11/2 8:34 上午
  **/
 public class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) { val = x; }
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
 }
--- src/exercise/medium/_002/Solution.java
+++ src/exercise/medium/_002/Solution.java
@@ -49,8 +49,35 @@ public class Solution {
      *     ListNode(int x) { val = x; }
      * }
      */
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-
+    public ListNode addTwoNumbers1(ListNode l1, ListNode l2) {
+        ListNode head = null;
+        ListNode tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            // 相加
+            int sum = n1 + n2 + carry;
+            if (head == null) {
+                // sum % 10 取余表示相加之后得到的数
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            // 进位的数
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
+        }
+        return head;
     }
 
 }
