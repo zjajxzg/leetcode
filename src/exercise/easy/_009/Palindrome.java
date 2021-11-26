9cda4f43eac3c617f494512e94e9a65cad47a07e
zjajxzg <zjajxzg@163.com>
2021-11-26 11:27:18 +0800

    feat: 9. 回文数

--- src/exercise/easy/_009/Palindrome.java
+++ src/exercise/easy/_009/Palindrome.java
@@ -1,7 +1,39 @@
 package exercise.easy._009;
 
 /**
- * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ * 9. 回文数
+ * 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+ *
+ * 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。例如，121 是回文，而 123 不是。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：x = 121
+ * 输出：true
+ * 示例 2：
+ *
+ * 输入：x = -121
+ * 输出：false
+ * 解释：从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+ * 示例 3：
+ *
+ * 输入：x = 10
+ * 输出：false
+ * 解释：从右向左读, 为 01 。因此它不是一个回文数。
+ * 示例 4：
+ *
+ * 输入：x = -101
+ * 输出：false
+ *
+ *
+ * 提示：
+ *
+ * -231 <= x <= 231 - 1
+ *
+ *
+ * 进阶：你能不将整数转为字符串来解决这个问题吗？
  */
 public class Palindrome {
     /**
@@ -32,7 +64,22 @@ public class Palindrome {
         return z == xx;
     }
 
+    public static boolean isPalindrome2(int x) {
+        if (x < 0) {
+            return false;
+        }
+        // 除以10!=0
+        int reverse = 0;
+        int tem = x;
+        while (x != 0) {
+            reverse = reverse * 10 +  x % 10;
+            x /= 10;
+        }
+        return reverse == tem;
+    }
+
     public static void main(String[] args) {
         System.out.println(isPalindrome1(-121));
+        System.out.println(isPalindrome2(-121));
     }
 }
