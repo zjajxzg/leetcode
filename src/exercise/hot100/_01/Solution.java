c4866ae7705f2e106757a3f38e95eb7261cef0a3
zjajxzg <zjajxzg@163.com>
2022-04-05 18:24:13 +0800

    feat: hot100 01. 两数之和

---
+++ src/exercise/hot100/_01/Solution.java
@@ -0,0 +1,61 @@
+package exercise.hot100._01;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 两数之和
+ *
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ *
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ *
+ * 你可以按任意顺序返回答案。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ * 示例 2：
+ *
+ * 输入：nums = [3,2,4], target = 6
+ * 输出：[1,2]
+ * 示例 3：
+ *
+ * 输入：nums = [3,3], target = 6
+ * 输出：[0,1]
+ *  
+ *
+ * 提示：
+ *
+ * 2 <= nums.length <= 104
+ * -109 <= nums[i] <= 109
+ * -109 <= target <= 109
+ * 只会存在一个有效答案
+ * 进阶：你可以想出一个时间复杂度小于 O(n2) 的算法吗？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author xuzhigang
+ * @date 2022/4/5 6:20 下午
+ **/
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // 利用map存储结果和索引
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            Integer result = map.get(target - nums[i]);
+            if (result != null && result != i) {
+                return new int[]{i, result};
+            }
+        }
+        return new int[]{0, 0};
+    }
+}
