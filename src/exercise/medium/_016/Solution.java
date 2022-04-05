72a46320d3616927e53786c5509e4e25e3aca3fa
zjajxzg <zjajxzg@163.com>
2022-04-05 18:19:56 +0800

    feat: 20. 有效的括号

---
+++ src/exercise/medium/_016/Solution.java
@@ -0,0 +1,59 @@
+package exercise.medium._016;
+
+import java.util.Arrays;
+
+/**
+ * 给你一个长度为 n 的整数数组 nums 和 一个目标值 target。请你从 nums 中选出三个整数，使它们的和与 target 最接近。
+ *
+ * 返回这三个数的和。
+ *
+ * 假定每组输入只存在恰好一个解。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [-1,2,1,-4], target = 1
+ * 输出：2
+ * 解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2) 。
+ * 示例 2：
+ *
+ * 输入：nums = [0,0,0], target = 1
+ * 输出：0
+ *  
+ *
+ * 提示：
+ *
+ * 3 <= nums.length <= 1000
+ * -1000 <= nums[i] <= 1000
+ * -104 <= target <= 104
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/3sum-closest
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author xuzhigang
+ * @date 2022/1/5 9:34 下午
+ **/
+public class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        // 排序
+        Arrays.sort(nums);
+        // 双指针
+        for (int i = 0; i < nums.length; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int result = nums[left] + nums[right] + i - target;
+                if (result == 0) {
+                    return result;
+                }
+                if (result > 0) {
+                    right--;
+                }
+            }
+        }
+        // todo
+        return 0;
+    }
+}
