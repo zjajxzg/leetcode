6232b23f44adae407cab47eccb2b7d9b01d4ed75
zjajxzg <zjajxzg@163.com>
2022-05-23 08:49:50 +0800

    feat: leetcode 94. 二叉树的中序遍历 递归

---
+++ src/exercise/hot100/_94/Solution.java
@@ -0,0 +1,47 @@
+package exercise.hot100._94;
+
+import common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 94. 二叉树的中序遍历
+ * 给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,null,2,3]
+ * 输出：[1,3,2]
+ * 示例 2：
+ *
+ * 输入：root = []
+ * 输出：[]
+ * 示例 3：
+ *
+ * 输入：root = [1]
+ * 输出：[1]
+ *
+ * @author xuzhigang
+ * @date 2022/5/22 8:06 下午
+ **/
+public class Solution {
+    // 递归版 中序遍历就是  左->根->右
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorder(root, res);
+        return res;
+    }
+
+    public void inorder(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, res);
+        res.add(root.val);
+        inorder(root.right, res);
+    }
+}
