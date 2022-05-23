4c21ae9028adbdbaf92205ff403cf6165bcb57ca
zjajxzg <zjajxzg@163.com>
2022-05-23 13:01:26 +0800

    feat: leetcode 141. 环形链表

---
+++ src/exercise/hot100/_141/Solution.java
@@ -0,0 +1,80 @@
+package exercise.hot100._141;
+
+import common.ListNode;
+
+import java.util.HashSet;
+
+/**
+ * 给你一个链表的头节点 head ，判断链表中是否有环。
+ *
+ * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。注意：pos 不作为参数进行传递 。仅仅是为了标识链表的实际情况。
+ *
+ * 如果链表中存在环 ，则返回 true 。 否则，返回 false 。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ *
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ * 示例 2：
+ *
+ *
+ *
+ * 输入：head = [1,2], pos = 0
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ * 示例 3：
+ *
+ *
+ *
+ * 输入：head = [1], pos = -1
+ * 输出：false
+ * 解释：链表中没有环。
+ *  
+ *
+ * 提示：
+ *
+ * 链表中节点的数目范围是 [0, 104]
+ * -105 <= Node.val <= 105
+ * pos 为 -1 或者链表中的一个 有效索引 。
+ *
+ *
+ *
+ * @author xuzhigang
+ * @date 2022/5/23 8:50 上午
+ **/
+public class Solution {
+    // 快慢指针 两个指针一快一慢，如果有环，总会相遇
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode quick = head.next;
+        ListNode slow = head;
+
+        while (quick != slow) {
+            if (quick == null || quick.next == null) {
+                return false;
+            }
+            quick = quick.next.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+
+    public boolean hasCycle2(ListNode head) {
+        HashSet<ListNode> set = new HashSet();
+        while (head != null) {
+            set.add(head);
+            head = head.next;
+            if (set.contains(head)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
